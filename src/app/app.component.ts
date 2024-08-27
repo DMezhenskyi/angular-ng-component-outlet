4787c2d1d1bc0e241155d40945b3cd264e3d8da6
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-08-27 11:27:08 +0200

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,46 @@
-import { Component, ComponentRef, viewChild, ViewContainerRef } from "@angular/core";
+import { Component, ComponentRef, inject, TemplateRef, Type, viewChild, ViewContainerRef } from "@angular/core";
 import { WidgetComponent } from "./widget/widget.component";
+import { NgComponentOutlet } from "@angular/common";
+import { WeatherContentComponent } from "./widget/weather-content.component";
 @Component({
   selector: "app-root",
   standalone: true,
   template: `
     <img class="logo" src="./logo.svg" alt="Decoded Frontend" />
     <h1 class="page-title">ngContentOutlet Demo</h1>
-    
+    <ng-template #contentTpl>
+      <app-weather-content />
+    </ng-template>
     <main id="content">
-      <ng-container #container></ng-container>
+      <ng-container
+        [ngComponentOutlet]="component"
+        [ngComponentOutletInputs]="componentInputs"
+        [ngComponentOutletContent]="content"></ng-container>
       <section class="toolbar">
         <button (click)="createComponent()" class="create">Create Component</button>
         <button (click)="destroyComponent()" class="destroy">Destroy Component</button>
       </section>
     </main>
   `,
+  imports: [NgComponentOutlet, WeatherContentComponent]
 })
 export class AppComponent {
-  container = viewChild('container', {read: ViewContainerRef})
-  #componentRef?: ComponentRef<WidgetComponent>
-  
+  vcr = inject(ViewContainerRef);
+  contentTpl = viewChild<TemplateRef<unknown>>('contentTpl')
+  protected content: Node[][] = [];
+  protected component: Type<WidgetComponent> | null = null;
+  protected componentInputs = {
+    title: 'Weather',
+    description: 'Currently in Vienna:'
+  }
+   
   createComponent() {
-    this.#componentRef = this.container()?.createComponent(WidgetComponent);
+    this.content = [
+      this.vcr.createEmbeddedView(this.contentTpl()!).rootNodes
+    ]
+    this.component = WidgetComponent;
   }
   destroyComponent() {
-    this.#componentRef?.destroy();
+    this.component = null;
   }
 }
